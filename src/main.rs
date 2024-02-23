a40061f4c9b9c5aca9a5c2305183b188accb538b
Thomas Plisson <thomas.plisson@epitech.eu>
2024-02-23 20:23:08 +0100

    DB ebauche

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::collections::HashMap;
-use std::time::Duration;
 use std::net::SocketAddr;
+use std::time::Duration;
 
-use hyper::{server::conn::http1, service::service_fn, Response};
-use massa_models::slot::Slot;
-use massa_sdk::{Client, ClientConfig, HttpConfig};
-use massa_time::MassaTime;
 use http_body_util::Full;
-use hyper::Request;
 use hyper::body::Bytes;
+use hyper::Request;
+use hyper::{server::conn::http1, service::service_fn, Response};
 use hyper_util::rt::TokioIo;
+use massa_models::slot::{Slot, SlotSerializer};
+use massa_sdk::{Client, ClientConfig, HttpConfig};
+use massa_serialization::Serializer;
+use massa_time::MassaTime;
+use rocksdb::DB;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let db = DB::open_default("./db").unwrap();
+    let slot_ser = SlotSerializer::new();
+    let last_saved_slot: Option<Slot> = None;
+
     tokio::spawn(async move {
         let config = HttpConfig {
             client_config: ClientConfig {
@@ -39,11 +45,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .await
         .unwrap();
         loop {
-            // Check last saved slot DB (soit l'info dpeuis la db soit depuis une variable)
+            let range_start = last_saved_slot.unwrap_or(Slot::new(0, 0));
             let get_status = client.public.get_status().await.unwrap();
-            let _last_slot = get_status.last_slot.unwrap();
-            // Make range last saved slot DB/last executed slot node
-            let _slots_transfers = client.public.get_slots_transfers(vec![]).await.unwrap();
+            let range_end = get_status.last_slot.unwrap();
+            // TODO: generate slots from range_start to range_end
+            let slots: Vec<Slot> = vec![range_start, range_end];
+            let slots_transfers = client
+                .public
+                .get_slots_transfers(slots.clone())
+                .await
+                .unwrap();
+            for (currents, slot) in slots_transfers.iter().zip(slots.iter()) {
+                let mut raw_slot = vec![];
+                slot_ser.serialize(&slot, &mut raw_slot).unwrap();
+                for (index, transfer) in currents.iter().enumerate() {
+                    let mut raw_transfer: Vec<u8> = vec![];
+                    // TODO: implement transfer serializer
+                    let key = [raw_slot.clone(), index.to_be_bytes().to_vec()].concat();
+                    // TODO: save raw transfer to DB
+                    let _ = db.put(key, []);
+                }
+            }
             // Save DB
             tokio::time::sleep(Duration::from_millis(500)).await;
         }
@@ -76,24 +98,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
 }
 
-async fn transfers(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
-    let params = form_urlencoded::parse(&req.uri().query().unwrap().as_bytes()).collect::<HashMap<_, _>>();
+async fn transfers(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
+    let params =
+        form_urlencoded::parse(&req.uri().query().unwrap().as_bytes()).collect::<HashMap<_, _>>();
     let slot = params.get("slot");
     let Some(slot) = slot else {
-        return Response::builder().status(400).body(Full::new(Bytes::from("Slot not found")));
+        return Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Slot not found")));
     };
     let slot = slot.split('_').collect::<Vec<&str>>();
     if slot.len() != 2 {
-        return Response::builder().status(400).body(Full::new(Bytes::from("Invalid slot")));
+        return Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Invalid slot")));
     }
     let Ok(period) = slot[0].parse::<u64>() else {
-        return Response::builder().status(400).body(Full::new(Bytes::from("Invalid period")));
+        return Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Invalid period")));
     };
     let Ok(thread) = slot[1].parse::<u8>() else {
-        return Response::builder().status(400).body(Full::new(Bytes::from("Invalid thread")));
+        return Response::builder()
+            .status(400)
+            .body(Full::new(Bytes::from("Invalid thread")));
     };
     let slot = Slot::new(period, thread);
-    // Fetch slot infos from DB
     println!("Slot: {:?}", slot);
+    // TODO: pass DB instance to struct with this as member function
+    // TODO: fetch transfers from DB with slot prefix iterator
+    // TODO: implement transfer deserializer
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
