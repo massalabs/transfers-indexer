a480d32abbc7063784380f7d9d4fc3754f91277b
AurelienFT <aurelien.foucault@epitech.eu>
2024-02-26 11:31:32 +0100

    Add logic to save slots

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::time::Duration;
 
 use http_body_util::Full;
@@ -7,19 +8,18 @@ use hyper::body::Bytes;
 use hyper::Request;
 use hyper::{server::conn::http1, service::service_fn, Response};
 use hyper_util::rt::TokioIo;
-use massa_models::slot::{Slot, SlotSerializer};
+use massa_api_exports::execution::Transfer;
+use massa_models::address::Address;
+use massa_models::amount::Amount;
+use massa_models::slot::Slot;
 use massa_sdk::{Client, ClientConfig, HttpConfig};
-use massa_serialization::Serializer;
 use massa_time::MassaTime;
-use rocksdb::DB;
+use mysql::prelude::Queryable;
+use mysql::Pool;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let db = DB::open_default("./db").unwrap();
-    let slot_ser = SlotSerializer::new();
-    let last_saved_slot: Option<Slot> = None;
-
     tokio::spawn(async move {
         let config = HttpConfig {
             client_config: ClientConfig {
@@ -44,27 +44,84 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         )
         .await
         .unwrap();
+        let url = "mysql://user:password@localhost:3307/db";
+        let pool = Pool::new(url).unwrap();
+
+        let mut conn = pool.get_conn().unwrap();
+        conn.query_drop(
+            "CREATE TABLE IF NOT EXISTS transfers (
+            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            slot varchar(100) not null,
+            from_addr varchar(100) not null,
+            to_addr varchar(100) not null,
+            amount bigint not null,
+            context text not null
+        )",
+        )
+        .unwrap();
+        conn.query_drop(
+            "CREATE TABLE IF NOT EXISTS metadata (
+            id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+            key_text text not null,
+            value_text text not null
+        )",
+        )
+        .unwrap();
+
+        let mut last_saved_slot = match conn
+            .query_first::<String, _>(
+                "SELECT value_text FROM metadata WHERE key_text = 'last_slot'",
+            )
+            .unwrap()
+        {
+            Some(res) => {
+                let parts: Vec<&str> = res.split('_').collect();
+                Slot::new(parts[0].parse().unwrap(), parts[1].parse().unwrap())
+            }
+            None => {
+                let get_status = client.public.get_status().await.unwrap();
+                let last_slot_api = get_status.execution_stats.final_cursor;
+                conn.exec_drop(
+                    "INSERT INTO metadata (key_text, value_text) VALUES (?, ?)",
+                    (
+                        "last_slot",
+                        format!("{}_{}", last_slot_api.period, last_slot_api.thread),
+                    ),
+                )
+                .unwrap();
+                last_slot_api
+            }
+        };
         loop {
-            let range_start = last_saved_slot.unwrap_or(Slot::new(0, 0));
             let get_status = client.public.get_status().await.unwrap();
-            let range_end = get_status.last_slot.unwrap();
-            // TODO: generate slots from range_start to range_end
-            let slots: Vec<Slot> = vec![range_start, range_end];
+            let range_end = get_status.execution_stats.final_cursor;
+            let mut last_inserted_slot = last_saved_slot;
+            let mut slots = vec![];
+            while range_end != last_inserted_slot {
+                last_inserted_slot = last_inserted_slot.get_next_slot(32).unwrap();
+                slots.push(last_inserted_slot);
+            }
             let slots_transfers = client
                 .public
                 .get_slots_transfers(slots.clone())
                 .await
                 .unwrap();
             for (currents, slot) in slots_transfers.iter().zip(slots.iter()) {
-                let mut raw_slot = vec![];
-                slot_ser.serialize(&slot, &mut raw_slot).unwrap();
-                for (index, transfer) in currents.iter().enumerate() {
-                    let mut raw_transfer: Vec<u8> = vec![];
-                    // TODO: implement transfer serializer
-                    let key = [raw_slot.clone(), index.to_be_bytes().to_vec()].concat();
-                    // TODO: save raw transfer to DB
-                    let _ = db.put(key, []);
+                for transfer in currents.iter() {
+                    conn.exec_drop("INSERT INTO transfers (slot, from_addr, to_addr, amount, context) VALUES (?, ?, ?, ?, ?)", (
+                        format!("{}_{}", slot.period, slot.thread),
+                        transfer.from.to_string(),
+                        transfer.to.to_string(),
+                        transfer.amount.to_raw(),
+                        serde_json::to_string(&transfer.context).unwrap()
+                    )).unwrap();
                 }
+                last_saved_slot = *slot;
+                conn.exec_drop(
+                    "UPDATE metadata SET value_text = ? WHERE key_text = 'last_slot'",
+                    (format!("{}_{}", slot.period, slot.thread),),
+                )
+                .unwrap();
             }
             // Save DB
             tokio::time::sleep(Duration::from_millis(500)).await;
@@ -101,6 +158,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 async fn transfers(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<Full<Bytes>>, hyper::http::Error> {
+    let url = "mysql://user:password@localhost:3307/db";
+    let pool = Pool::new(url).unwrap();
+
+    let mut conn = pool.get_conn().unwrap();
     let params =
         form_urlencoded::parse(&req.uri().query().unwrap().as_bytes()).collect::<HashMap<_, _>>();
     let slot = params.get("slot");
@@ -126,9 +187,24 @@ async fn transfers(
             .body(Full::new(Bytes::from("Invalid thread")));
     };
     let slot = Slot::new(period, thread);
-    println!("Slot: {:?}", slot);
-    // TODO: pass DB instance to struct with this as member function
-    // TODO: fetch transfers from DB with slot prefix iterator
-    // TODO: implement transfer deserializer
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    let Ok(res) = conn.exec::<(String, String, u64, String), _, _>(
+        "SELECT from_addr, to_addr, amount, context FROM transfers WHERE slot = ?",
+        (format!("{}_{}", slot.period, slot.thread),),
+    ) else {
+        return Response::builder()
+            .status(500)
+            .body(Full::new(Bytes::from("Internal error")));
+    };
+    let mut transfers = Vec::new();
+    for transfer in res {
+        transfers.push(Transfer {
+            from: Address::from_str(&transfer.0).unwrap(),
+            to: Address::from_str(&transfer.1).unwrap(),
+            amount: Amount::from_raw(transfer.2),
+            context: serde_json::from_str(&transfer.3).unwrap(),
+        });
+    }
+    Ok(Response::new(Full::new(Bytes::from(
+        serde_json::to_string(&transfers).unwrap(),
+    ))))
 }
